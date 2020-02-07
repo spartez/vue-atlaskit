b1cae231ed2269f8d56d033f3162256de39e5844
abilicz <adrian.bilicz@spartez.com>
2020-02-07 16:15:00 +0100

    top progress bar

---
+++ stories/Progress/Progress.story.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <Button appearance="primary" @click="start">
+            Start
+        </Button>
+        <Button @click="finish">
+            Finish
+        </Button>
+    </div>
+</template>
+
+<script>
+    import Progress from '@/components/Progress/progress';
+    import Button from '@/components/Button/Button';
+
+    export default {
+        name: 'ProgressStory',
+        components: { Button },
+        data() {
+            return {
+                progress: undefined
+            };
+        },
+        mounted() {
+            this.progress = new Progress();
+        },
+        methods: {
+            start() {
+                this.progress.start();
+            },
+            finish() {
+                this.progress.finish();
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
