80137f1a2e2c73984135f376112ad658bb4b4450
abilicz <adrian.bilicz@spartez-software.com>
2020-05-07 12:03:37 +0200

    added append to body prop to tooltip

--- src/components/Tooltip/Tooltip.vue
+++ src/components/Tooltip/Tooltip.vue
@@ -2,10 +2,11 @@
     <div class="tooltip">
         <div ref="target" class="target" @mouseenter="onMouseEnter"
              @mouseleave="onMouseLeave">
-            <slot />
+            <slot/>
         </div>
         <transition name="fade">
             <Popper v-if="show" ref="popper" :transition-delay="300"
+                    :boundaries-element="boundariesElement"
                     :target-element="$refs.target" :placement="placement" :offset="offset">
                 <span ref="label" class="label" :disabled="disabled">{{ label }}</span>
             </Popper>
@@ -39,6 +40,10 @@
             appendToBody: {
                 type: Boolean,
                 default: false
+            },
+            boundariesElement: {
+                type: String,
+                default: 'viewport'
             }
         },
         data() {
@@ -56,6 +61,9 @@
                 }
             },
             onMouseLeave() {
+                if (this.appendToBody) {
+                    document.body.removeChild(this.$refs.label);
+                }
                 this.show = false;
             },
             append() {
@@ -72,29 +80,31 @@
 
 <style scoped>
 .tooltip {
-  display: inline-block;
+    display: inline-block;
 }
 
 .label {
-  box-sizing: border-box;
-  padding: 2px 10px;
-  font-size: 12px;
-  font-weight: 400;
-  box-shadow: 0 1px 2px 1px rgba(0, 1, 0, 0.2);
-  white-space: nowrap;
-  pointer-events: none;
-  background-color: #172b4d;
-  border-radius: 3px;
-  color: #ffffff;
+    box-sizing: border-box;
+    position: absolute;
+    z-index: 999999;
+    padding: 2px 10px;
+    font-size: 12px;
+    font-weight: 400;
+    box-shadow: 0 1px 2px 1px rgba(0, 1, 0, 0.2);
+    white-space: nowrap;
+    pointer-events: none;
+    background-color: #172b4d;
+    border-radius: 3px;
+    color: #ffffff;
 }
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.3s;
+    transition: opacity 0.3s;
 }
 
 .fade-enter,
 .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
