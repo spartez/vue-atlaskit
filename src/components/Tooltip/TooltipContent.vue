f9a78bd5859e665b427f53f6efe937723f93d510
Michał Nykiel <michal.nykiel@spartez-software.com>
2021-07-15 16:12:56 +0200

    add tooltip directive

---
+++ src/components/Tooltip/TooltipContent.vue
@@ -0,0 +1,85 @@
+<template>
+    <transition name="fade" appear>
+        <Popper
+            :transition-delay="300"
+            :boundaries-element="boundariesElement"
+            :target-element="targetElement"
+            :placement="placement"
+            :offset="offset"
+            :with-delay="withDelay">
+            <span class="label"
+                  :disabled="disabled">
+                {{ label }}
+            </span>
+        </Popper>
+    </transition>
+</template>
+
+<script>
+    import Popper from '../Popper/Popper.vue';
+
+    export default {
+        components: { Popper },
+        props: {
+            label: {
+                type: String,
+                required: true
+            },
+            placement: {
+                type: String,
+                default: 'bottom'
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            offset: {
+                type: String,
+                default: '0,5'
+            },
+            boundariesElement: {
+                type: String,
+                default: 'viewport'
+            },
+            targetElement: {
+                type: HTMLElement,
+                required: true
+            },
+            withDelay: {
+                type: Boolean,
+                default: true
+            }
+        }
+    };
+</script>
+
+<style scoped>
+.label {
+    box-sizing: border-box;
+    position: absolute;
+    z-index: 999999;
+    padding: 2px 10px;
+    font-size: 12px;
+    font-weight: 400;
+    box-shadow: 0 1px 2px 1px rgba(0, 1, 0, 0.2);
+    white-space: nowrap;
+    pointer-events: none;
+    background-color: #172b4d;
+    border-radius: 3px;
+    color: #ffffff;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .3s;
+}
+
+.fade-enter-active[with-delay] {
+    transition-delay: .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
