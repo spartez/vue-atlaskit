0c37f6cfe751f5b8fbc03ccb151b7951544ea3a4
mwisniewski2000 <108661625+mwisniewski2000@users.noreply.github.com>
2023-08-22 10:21:34 +0200

    APJ-2864 Add Breadcrumbs (#92)
    * APJ-2864 Add Breadcrumbs
    
    * APJ-2864 fix review comments
    
    ---------
    
    Co-authored-by: mwisniewski2000 <maciek.wisniewski@appfire.com>

---
+++ src/components/Breadcrumbs/BreadcrumbItem.vue
@@ -0,0 +1,96 @@
+<template>
+  <div
+    class="breadcrumb"
+    :with-icon="!!$slots.icon"
+  >
+    <span class="item">
+      <slot name="icon"/>
+      <slot name="link">
+        <a
+          :href="link"
+          :target="target"
+        >{{ text }}</a>
+      </slot>
+    </span>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'BreadcrumbItem',
+        props: {
+            link: {
+                type: String,
+                default: '#'
+            },
+            text: {
+                type: String,
+                default: ''
+            },
+            target: {
+                type: String,
+                default: '_self'
+            }
+        }
+    };
+</script>
+
+<style scoped>
+.breadcrumb {
+    min-width: 45px;
+    display: flex;
+    align-items: center;
+    flex-shrink: 10;
+    transition: all ease-out .3s;
+}
+
+.breadcrumb[with-icon="true"] {
+    min-width: 60px;
+}
+
+.breadcrumb:first-of-type {
+    flex: 0 1 auto;
+}
+
+.item {
+    display: flex;
+    min-width: 0;
+    align-items: center;
+}
+
+.item >>> a {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    color: var(--ds-text-subtle, #6B778C);
+    margin-left: 0px;
+    display: block;
+}
+
+[with-icon="true"] >>> a {
+    margin-left: 5px;
+}
+
+.breadcrumb:after {
+    display: inline-block;
+    content: '/';
+    color: var(--ds-text-subtle, #6B778C);
+    text-align: center;
+    margin: 0px 6px;
+    width: 8px;
+}
+
+.breadcrumb:last-of-type:after {
+    content: none;
+}
+
+.breadcrumb:last-child {
+    flex: 0 1 auto;
+}
+
+.breadcrumb:hover {
+    flex: 1 0 auto;
+    opacity: 1;
+    transition: all ease-out .3s;
+}
+</style>
